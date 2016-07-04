774725b404c3e2dcefb6c98cf02498b8de0a573f
Ankur Jain <ankurjain@Ankurs-MacBook-Pro.local>
2016-07-04 17:50:05 +0530

    added comment

--- website/urls.py
+++ website/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('music.urls')),
 
-    # url(r'^music/', admin.site.urls),
+
+    #add url(r'^music/', admin.site.urls),
 ]
