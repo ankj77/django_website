cc75c15aa5df0522d50c60d79bba8d6ddadd0740
Ankur Jain <ankurjain@Ankurs-MacBook-Pro.local>
2016-07-04 17:35:58 +0530

    added model in music

--- music/models.py
+++ music/models.py
@@ -1,5 +1,18 @@
-from __future__ import unicode_literals
+
+from django.db import  models
 
 from django.db import models
 
 # Create your models here.
+
+class Album (models.Model) :
+    artist = models.CharField(max_length = 50)
+    album_title = models.CharField(max_length = 50)
+    genre = models.CharField (max_length = 50)
+    album_logo = models.CharField(max_length=50)
+
+class Song(models.Model) :
+    album  = models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type = models.CharField(max_length = 10)
+    song_title = models.CharField(max_length = 50)
+
