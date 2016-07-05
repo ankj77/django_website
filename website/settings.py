6b004c13a7c5e20e2a7ddf40645afeaec16e238e
ankur jain <ankj77@gmail.com>
2016-07-05 18:37:41 +0530

    added code in polls

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'music.apps.MusicConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
