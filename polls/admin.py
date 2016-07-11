4652dd03500755ddf3e4211c898899f1baf00838
ankur jain <ankj77@gmail.com>
2016-07-11 21:26:07 +0530

    till tutorial 6

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,14 @@
+
+
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
