a5c9fd33a852f5e6f045f7222283431d2b9e06a6
ankur jain <ankj77@gmail.com>
2016-07-12 14:42:14 +0530

    added code in music app

--- music/views.py
+++ music/views.py
@@ -4,17 +4,16 @@ from .models import Album
 # Create your views here.
 
 def index (request) :
-	albums = Album.objects.all()
-	html = ""
-	return  HttpResponse(html)
+    albums = Album.objects.all()
+    html = "<h1>Welcome to Music Album Page</h1>"
+    for album in albums :
+        url = '/music/'+str(album.id)
+        html+=  '<a href="'+url+'">'+album.album_title+' By : '+ album.artist +'</a> <br>'
+    return HttpResponse(html)
 
-# for a in albums 
-# 	url = "/music/"+str(a.id) 
-# 	html+= '<a href = "'+url+'">' + url+ ' </a>'
 
 
 def detail(request,album_id):
 
-	html = ""
-	return HttpResponse("<h2>detail of album_id "+str(album_id)+"</h2>")
-	
+    html = ""
+    return HttpResponse("<h2> Detail of Album Id : "+str(album_id)+"</h2>")
